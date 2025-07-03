2804903458990a6ed88478630f01ee744739f030
evulan <28766596+evulan@users.noreply.github.com>
2025-07-03 11:13:50 +0200

    spelling

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='kimmdy_paper_theme',                    # Package name (shown on PyPI)
     version='0.1',
-    description='Theme elements for the kimmdy emulato paper',
+    description='Theme elements for the kimmdy emulator paper',
     author='BMM',
     packages=find_packages(),          # Automatically finds packages in this folder
     install_requires=[],               # Add dependencies here if needed
